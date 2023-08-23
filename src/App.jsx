f76dbc1ace19516ee30896ea68951397fada4dad
gradfordl <laurengradford@gmail.com>
2023-08-22 21:28:39 -0400

    App.js-- mapped through array to create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { Route, Routes } from "react-router-dom";
-import { useState } from 'react'
-import Pokemon from './components/Pokemon'
-import './App.css'
 import StartQuiz from './components/StartQuiz'
 import QuestionCard from "./components/QuestionCard";
 import Header from "./components/Header";
+import './App.css'
+import questions from "./data";
 
-function App() {
 
+function App(props) {
+  console.log(Routes)
   return (
     <div className='App'>
       <Header/>
       <StartQuiz/>
       <Routes>
-        <Route path="questions/:id" element={<QuestionCard/>}/>
+      {questions.map(question =>
+        <Route
+          key={question.id}
+          path="/questions/:id"
+          element={<QuestionCard />}
+        />)}
       </Routes>
+     
   
 
 
@@ -25,7 +31,6 @@ function App() {
 
 
 
-      <Pokemon/>
     </div>
   )
 }
--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -2,27 +2,43 @@ import { useParams, Link } from "react-router-dom";
 import questions from "../data";
 
 function QuestionCard() {
-    const params = useParams();
+  const params = useParams();
 
-    questions.find((question) => {
-        return params.id == question.id
-    })
-    
+  questions.find((question) => {
+    console.log(question.text);
+    return params.id == question.id;
+  });
+  //need to display individual question text and options on screen
+
   return (
-<div className='question-card'>
-<h2>Question Goes Here!</h2>
-{/* use <Link/> & onClick on link*/ }
-<Link to="/questions/2">OPTION 1</Link>
-
-{/* <ul>
+    <div className="question-card">
+      <h2>QUESTION TEXT</h2>
+      {/* use <Link/> & onClick on link*/}
+      {/* <Link to={`/questions/${id}`}> ????? */}
+      {/* //initial setup for routes to questions */}
+      <Link to="/questions/2">
+        <div>QUESTION OPTION TEXT</div>
+      </Link>
+      <Link to="/questions/3">
+        <div>QUESTION OPTION TEXT</div>
+      </Link>
+      <Link to="/questions/4">
+        <div>QUESTION OPTION TEXT</div>
+      </Link>
+      <Link to="/questions/5">
+        <div>QUESTION OPTION TEXT</div>
+      </Link>
+      <Link to="/questions/6">
+        <div>QUESTION OPTION TEXT</div>
+      </Link>
+      {/* <ul>
   <li>Option 1</li>
   <li>Option 2</li>
   <li>Option 3</li>
   <li>Option 4</li>
 </ul> */}
-
-</div>
-  )
+    </div>
+  );
 }
 
-export default QuestionCard
+export default QuestionCard;
